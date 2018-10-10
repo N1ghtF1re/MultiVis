b28c9279e1174034e3c9f3902bd0a61e2f0b1d0f
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-10-10 22:04:19 +0300

    upgrade popup style

--- src/main/resources/static/js/popup.js
+++ src/main/resources/static/js/popup.js
@@ -1,26 +1,38 @@
 "use strict";
 
-window.onload = function() {
-    var popup = document.createElement('div');
-    popup.id = 'popup-window';
-    popup.style = "display: none; opacity: 0";
+const CLOSE_STYLE = "top: -50%; opacity: 0";
+const SHOW_STYLE = "";
 
-    var title = document.createElement('h1');
+window.onload = function() {
+    let popup = document.createElement('div');
+    popup.id = 'popup-window';
+    popup.style = CLOSE_STYLE;
+
+    let title = document.createElement('h1');
     title.id="popup-title";
 
     popup.appendChild(title);
 
-    var content = document.createElement('p');
+    let content = document.createElement('p');
     content.id="popup-content";
 
     popup.appendChild(content);
 
+    let btnClose = document.createElement("button");
+    btnClose.innerText = "Закрыть";
+
+    btnClose.onclick = function () {
+        popup.style = CLOSE_STYLE;
+    };
+
+    popup.appendChild(btnClose);
+
     document.body.appendChild(popup);
-}
+};
 
 function showPopup(content, title) {
     let popupWindow = document.getElementById("popup-window");
-    popupWindow.style = "display: block; opacity:1";
+    popupWindow.style = SHOW_STYLE;
 
     let contentEl = document.getElementById('popup-content');
     contentEl.innerHTML = content;
@@ -36,7 +48,7 @@ function showSituatuinsPopup(regionName, sitArr, sitNames, basicColors) {
     contentEl.innerHTML = '';
 
     let contentUl = document.createElement("ul");
-    for(var i = 0; i < sitArr.length; i++) {
+    for(let i = 0; i < sitArr.length; i++) {
         let newLi = document.createElement("li");
 
         let colorBlock = document.createElement("div");
@@ -45,15 +57,19 @@ function showSituatuinsPopup(regionName, sitArr, sitNames, basicColors) {
 
         let sitNameEl = document.createElement("span");
         sitNameEl.className = "sitname";
-        sitNameEl.innerHTML = sitNames[i]['name'];
+        sitNameEl.innerHTML = sitNames[i]['name'] + " - ";
 
         let sitCountEl = document.createElement("span");
         sitCountEl.className = "sitcount";
         sitCountEl.innerHTML  = sitArr[i];
 
+        let strTimes = document.createElement("span");
+        strTimes.innerText = " раз";
+
         newLi.appendChild(colorBlock);
         newLi.appendChild(sitNameEl);
         newLi.appendChild(sitCountEl);
+        newLi.appendChild(strTimes);
 
         contentUl.appendChild(newLi);
     }
@@ -62,5 +78,5 @@ function showSituatuinsPopup(regionName, sitArr, sitNames, basicColors) {
     let titleEl = document.getElementById('popup-title');
     titleEl.innerHTML = regionName;
 
-    popupWindow.style = 'display: block; opacity:1';
+    popupWindow.style = SHOW_STYLE;
 }
