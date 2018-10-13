89c2c23582d54c626b87488d0f76a7102a9b234c
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-10-13 14:54:35 +0300

    Added getting the number of situations through api

--- src/main/java/men/brakh/emergencymap/db/SituationsRepository.java
+++ src/main/java/men/brakh/emergencymap/db/SituationsRepository.java
@@ -3,8 +3,11 @@ package men.brakh.emergencymap.db;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SituationsRepository extends CrudRepository<Situations, Integer> {
 
+    @Query("SELECT COUNT(e) FROM Situations e")
+    public int getSitCount();
 }
