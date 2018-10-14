b0ec273024a37201d1cf13b842c2e4f3eb13f45b
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-10-14 16:30:18 +0300

    Added api page

--- src/main/java/men/brakh/emergencymap/controllers/MainController.java
+++ src/main/java/men/brakh/emergencymap/controllers/MainController.java
@@ -1,8 +1,11 @@
 package men.brakh.emergencymap.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -10,6 +13,17 @@ public class MainController {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index() {
     return "index.html";
+  }
+
+  @RequestMapping(value = "/api", method = RequestMethod.GET)
+  public String api(HttpServletRequest request, Model model) {
+
+    String baseUrl = String.format("%s://%s:%d",request.getScheme(),  request.getServerName(), request.getServerPort());
+
+
+    model.addAttribute("host", baseUrl);
+
+    return "api";
   }
 
 }
